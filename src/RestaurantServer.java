e07ef588110f7930c265861d480be23a2aa73099
riley98-yorku <riley98@my.yorku.ca>
2023-02-07 15:16:01 -0500

    v2

--- src/RestaurantServer.java
+++ src/RestaurantServer.java
@@ -6,32 +6,21 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class RestaurantServer {
-    public static void main(String[] args) throws Exception {
         
-        final int PORT = 2000;
-        Socket clientSocket;
+    final int PORT = 2000;
+    Socket clientSocket;
+
+    public void server() {
+
         String order = "";
-
+        
         try {
-            ServerSocket serverSocket = new ServerSocket(PORT);
-            clientSocket = serverSocket.accept();
-
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            String newLine;
-            while((newLine = in.readLine()) != null) {
-                order += newLine;
+            while(true) {
+                ServerSocket serverSocket = new ServerSocket(PORT);
+                clientSocket = serverSocket.accept();
+                HandleClient handleClient = new HandleClient(clientSocket);
+                handleClient.start();
             }
-
-            out.write("Thank you for your order!\n");
-            out.flush();
-            in.close();
-            out.close();
-            clientSocket.close();
-            serverSocket.close();
-        } catch(IOException e) {}
-
-        System.out.println(order);
+        } catch(IOException e) {}    
     }
 }
