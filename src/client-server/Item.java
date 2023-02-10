1e556694f5030a149bc5e0848dd7bc52aadb401f
Ivan-Korpalo <29556366+Ivan-Korpalo@users.noreply.github.com>
2023-02-10 06:19:36 -0500

    Update Item.java
    Added a price parameter and all of the other methods that entails. The additional parameter is necessary for certain functionality. I made an effort to keep the style consistent.

--- src/client-server/Item.java
+++ src/client-server/Item.java
@@ -2,9 +2,16 @@
 public class Item {
 
 	String name;
+	int price; //200 is equivalent to $2.00
 	
 	Item(String name){
 		this.name = name;
+		this.price = 0;
+	}
+	
+	Item(String name, int price){
+		this.name = name;
+		this.price = price;
 	}
 	
 	// GETTERS
@@ -12,9 +19,17 @@ public class Item {
 		return this.name;
 	}
 	
+	public int getPrice() {
+		return this.price;
+	}
+	
 	// SETTERS
 	private String setName(String name) {
 		return this.name = name;
+	}
+	
+	private int setPrice(int price) {
+		return this.price = price;
 	}
 	
 }
