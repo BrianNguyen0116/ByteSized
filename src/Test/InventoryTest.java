1124d3b60f32ebeafcf99ce738412f1c66155667
riley98-yorku <riley98@my.yorku.ca>
2023-03-10 01:34:32 -0500

    Bigger test class for Inventory

--- src/Test/InventoryTest.java
+++ src/Test/InventoryTest.java
@@ -6,12 +6,27 @@ import org.junit.jupiter.api.Test;
 class InventoryTest {
 
 	@Test
-	void testGetItem() {
+	void testSauce() {
 		Inventory inv = new Inventory();
-		
 		assertEquals(inv.getItem("Tomato Sauce").getTotal(), 100);
 		inv.getItem("Tomato Sauce").setTotal(25);
 		assertEquals(inv.getItem("Tomato Sauce").getTotal(), 25);
+	}
+
+	@Test
+	void testToppings() {
+		Inventory inv = new Inventory();
+		assertEquals(inv.getItem("Pepperoni").getTotal(), 100);
+		inv.getItem("Pepperoni").setTotal(25);
+		assertEquals(25, inv.getItem("Pepperoni").getTotal());
+	}
+
+	@Test
+	void testDough() {
+		Inventory inv = new Inventory();
+		assertEquals(inv.getItem("Stuffed").getTotal(), 100);
+		inv.getItem("Stuffed").setTotal(25);
+		assertEquals(25, inv.getItem("Stuffed").getTotal());
 	}
 
 }
