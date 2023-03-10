e4b5aca34bc36014186e754ede761fbe9fd2f7b3
Brian Nguyen <brian.nguyen0116@gmail.com>
2023-03-10 08:52:18 -0500

    Further Development (GUI)

---
+++ src/guiMain/RoundText.java
@@ -0,0 +1,28 @@
+import java.awt.Graphics;
+import java.awt.Shape;
+import java.awt.geom.RoundRectangle2D;
+import javax.swing.JLabel;
+
+public class RoundText extends JLabel {
+    private Shape shape;
+    
+    public RoundText(String s) {
+        setText(s);
+        setOpaque(false);
+    }
+    protected void paintComponent(Graphics g) {
+         g.setColor(getBackground());
+         g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+         super.paintComponent(g);
+    }
+    protected void paintBorder(Graphics g) {
+         g.setColor(getForeground());
+         g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+    }
+    public boolean contains(int x, int y) {
+         if (shape == null || !shape.getBounds().equals(getBounds())) {
+             shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+         }
+         return shape.contains(x, y);
+    }
+}
