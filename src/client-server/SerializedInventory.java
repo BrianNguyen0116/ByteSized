0843d1ebcfa7f01363b53f59875b132688de7919
riley98-yorku <riley98@my.yorku.ca>
2023-03-09 22:47:25 -0500

    serialInv removed unsused imports

--- src/client-server/SerializedInventory.java
+++ src/client-server/SerializedInventory.java
@@ -4,9 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 
 public class SerializedInventory extends Inventory {
 
